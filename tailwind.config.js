cc0efe28708640322197702d1f1a4ad12400a259
MonaBlanc <kim.mauritius@gmail.com>
2023-07-14 10:23:50 +0200

    configuration of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "primary dark": '#1f1f1f',
+        primary: '#ffffff',
+        highlight: {
+          dark: "#FFFFFF",
+          light: "#1f1f1f",
+        },
+        secondary: {
+          dark: "#707070",
+          light: "#e6e6e6",
+        },
+        action: "#3B82F6",
       },
     },
   },
