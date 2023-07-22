92ec8058b58a08c1bc5565abee8fd2f4426f0cc1
Kim <kim.mauritius@gmail.com>
2023-07-22 23:02:17 +0200

    smooth toggle transition

--- components/common/AdminNav.tsx
+++ components/common/AdminNav.tsx
@@ -51,14 +51,14 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
   return (
     <nav
       ref={navRef}
-      className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between"
+      className="h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark flex flex-col justify-between transition-width overflow-hidden"
     >
       <div>
         {/** Logo */}
         <Link href="/admin" className="flex items-center space-x-2 p-3">
           <Logo className="fill-highlight-light dark:fill-highlight-dark w-5 h-5" />
           {visible && (
-            <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold">
+            <span className="text-highlight-light dark:text-highlight-dark text-xl font-semibold leading-none">
               Admin
             </span>
           )}
@@ -74,7 +74,7 @@ const AdminNav: FC<Props> = ({ navItems }): JSX.Element => {
                     className="fill-highlight-light dark:fill-highlight-dark w-5 h-5"
                   />
                   {visible && (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={item.href} className="ml-2 leading-none">
                       {item.label}
                     </Link>
                   )}
