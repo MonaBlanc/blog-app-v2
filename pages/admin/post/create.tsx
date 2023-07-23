8e42dd86465cc7dfe9952a73e880a1a911bf2a9c
Kim <kim.mauritius@gmail.com>
2023-07-23 13:18:43 +0200

    implemented tiptap extension for text editor

---
+++ pages/admin/post/create.tsx
@@ -0,0 +1,10 @@
+import Editor from "@/components/editor";
+import { NextPage } from "next";
+
+interface Props {}
+
+const Create: NextPage<Props> = (): JSX.Element => {
+    return <Editor />;
+    }
+
+export default Create;
